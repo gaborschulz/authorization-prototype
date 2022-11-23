9010f72633b551e212b753686914cd33ab42ad0f
Gabor Schulz <gabor@gaborschulz.com>
2022-11-23 21:38:33 +0100

    Use GitHub Actions for linting and testing

--- authorization.py
+++ authorization.py
@@ -1,6 +1,5 @@
 import json
 import re
-from itertools import chain
 from datetime import datetime, timedelta
 
 USER_POLICY = [
@@ -27,25 +26,33 @@ ROLE_POLICY = [
     }
 ]
 
+
 def get_object() -> dict:
+    """Get object"""
     with open('object.json', 'r', encoding='utf-8') as fp:
         return json.load(fp)
 
+
 def get_statement_regex_object(statement: dict) -> dict:
+    """Get regex object"""
     result = {}
     result['Sid'] = statement['Sid']
     result['Effect'] = statement['Effect']
-    result['Resource'] = re.compile(statement.get('Resource').lower(), flags=re.IGNORECASE)
+    result['Resource'] = re.compile(statement.get('Resource', '').lower(), flags=re.IGNORECASE)
     result['Action'] = re.compile(statement['Action'].lower(), flags=re.IGNORECASE)
     return result
 
+
 def get_authorization_object(obj: dict) -> str:
+    """Get auth object"""
     result = json.dumps(obj).replace('"', '').replace(': ', ':').lower()
     return result
 
+
 def authorize(action: str, obj: dict, policy: list) -> tuple:
+    """Authorize"""
     authorized = False
-    
+
     # Prepare/compile statements
     deny_statements = [get_statement_regex_object(x) for x in policy if x['Effect'] == 'Deny']
     allow_statements = [get_statement_regex_object(x) for x in policy if x['Effect'] == 'Allow']
@@ -61,27 +68,30 @@ def authorize(action: str, obj: dict, policy: list) -> tuple:
     denied_by_action = [x for x in denied_by_resource if x['Action'].search(action)]
     if denied_by_action:
         return False, denied_by_action
-    
+
     allowed_by_action = [x for x in allowed_by_resource if x['Action'].search(action)]
     if allowed_by_action:
         authorized = True, allowed_by_action
 
     return authorized, None
 
+
 def main():
+    """Main function"""
     footprint = get_object()
     action = 'footprint:getlist'
-    
+
     iterations = 10
     start = datetime.now()
     policy = [*USER_POLICY, *ROLE_POLICY]
-    
+
+    authorized = False, None
     for i in range(iterations):
         authorized = authorize(action, footprint, policy)
-    
+
     end = datetime.now()
     elapsed = (end - start) / timedelta(milliseconds=1)
-    
+
     print(authorized)
     print(f'{elapsed:.3f} ms, {elapsed / iterations:.3f} ms per iteration')
 
